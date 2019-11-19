7efd29209a085d7e53de9cbb5882de8fe2f18bd4
e <591742716@qq.com>
2019-11-19 23:01:28 +0800

    :construction:

---
+++ t1/js/t.js
@@ -0,0 +1,6 @@
+"use strict";
+console.log("sdfsdf");
+function getDatass() {
+    console.log("第一步");
+}
+var str = "你好ts";
---
+++ t1/t.ts
@@ -0,0 +1,10 @@
+console.log("sdfsdf");
+
+function getDatass(){
+
+
+    console.log("第一步")
+
+}
+
+var str:string = "你好ts"
